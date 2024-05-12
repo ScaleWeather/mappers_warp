15233d06a8acd70ca6d407e52a51ad2e15ceea2e
Quba1 <22771850+Quba1@users.noreply.github.com>
2024-05-12 10:35:31 +0200

    remove lon lat from naming

--- src/lib.rs
+++ src/lib.rs
@@ -65,15 +65,15 @@ impl Warper {
     // this source pixel is just one among other several source pixels, and it might be possible that there are invalid
     // values in those other contributing source pixels. The weights used to take into account those invalid values
     // will be set to zero to ignore them.
-    pub fn warp(&self, lonlat_raster: &Array2<f64>) -> Result<Array2<f64>, WarperError> {
-        if lonlat_raster.shape()[0] != self.source_shape[0] as usize
-            || lonlat_raster.shape()[1] != self.source_shape[1] as usize
+    pub fn warp(&self, source_raster: &Array2<f64>) -> Result<Array2<f64>, WarperError> {
+        if source_raster.shape()[0] != self.source_shape[0] as usize
+            || source_raster.shape()[1] != self.source_shape[1] as usize
         {
             return Err(WarperError::InvalidRasterDimensions);
         }
 
         let target_raster = self.internals.map(|intr| {
-            let values = lonlat_raster.slice(s![
+            let values = source_raster.slice(s![
                 (intr.anchor_idx.1 - 1) as usize..(intr.anchor_idx.1 + 3) as usize,
                 (intr.anchor_idx.0 - 1) as usize..(intr.anchor_idx.0 + 3) as usize
             ]);
