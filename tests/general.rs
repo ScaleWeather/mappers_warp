5131658adf11a85af873db4385c0fe5f68db3246
Quba1 <22771850+Quba1@users.noreply.github.com>
2024-05-12 13:32:10 +0200

    add basic nan test

--- tests/general.rs
+++ tests/general.rs
@@ -6,7 +6,7 @@ use mappers::{
 };
 use ndarray::{Array2, Zip};
 use ndarray_stats::QuantileExt;
-use notgdalwarp::{CubicBSpline, MitchellNetravali, RasterBounds, Warper};
+use notgdalwarp::{raster_constant_pad, CubicBSpline, MitchellNetravali, RasterBounds, Warper};
 
 #[test]
 fn waves() -> Result<()> {
@@ -94,6 +94,67 @@ fn mitchell() -> Result<()> {
 
     assert!(target_raster.max()? <= source_raster.max()?);
     assert!(target_raster.min()? >= source_raster.min()?);
+
+    Ok(())
+}
+
+#[test]
+fn nan_padded_waves() -> Result<()> {
+    let src_proj = LongitudeLatitude;
+    let tgt_proj =
+        LambertConformalConic::new(80., 24., 12.472955, 35.1728044444444, Ellipsoid::WGS84)?;
+
+    let source_bounds = RasterBounds::new((59.25, 69.00), (31.00, 40.75), 0.25, 0.25, src_proj)?;
+    let target_bounds = RasterBounds::new(
+        (2_320_000. - 4_000_000., 2_740_000. - 4_000_000.),
+        (5_090_000. - 4_000_000., 5_640_000. - 4_000_000.),
+        10_000.,
+        10_000.,
+        tgt_proj,
+    )?;
+
+    let warper = Warper::initialize::<CubicBSpline, LongitudeLatitude, LambertConformalConic>(
+        &source_bounds,
+        &target_bounds,
+    )?;
+
+    let source_raster: Array2<f64> = ndarray_npy::read_npy("./tests/data/waves_34.npy")?;
+    let source_raster = raster_constant_pad(&source_raster, 3, f64::NAN);
+    let ref_raster: Array2<f64> = ndarray_npy::read_npy("./tests/data/waves_ref.npy")?;
+    let target_raster = warper.warp(&source_raster)?;
+
+    assert_eq!(target_raster.shape(), ref_raster.shape());
+    Zip::from(&target_raster)
+        .and(&ref_raster)
+        .map_collect(|&f, &o| assert_approx_eq!(f64, f, o, epsilon = 1e-6));
+
+    Ok(())
+}
+
+#[test]
+fn invalid_raster_size() -> Result<()> {
+    let src_proj = LongitudeLatitude;
+    let tgt_proj =
+        LambertConformalConic::new(80., 24., 12.472955, 35.1728044444444, Ellipsoid::WGS84)?;
+
+    let source_bounds = RasterBounds::new((60.00, 68.25), (31.75, 40.0), 0.25, 0.25, src_proj)?;
+    let target_bounds = RasterBounds::new(
+        (2_320_000. - 4_000_000., 2_740_000. - 4_000_000.),
+        (5_090_000. - 4_000_000., 5_640_000. - 4_000_000.),
+        10_000.,
+        10_000.,
+        tgt_proj,
+    )?;
+
+    let warper = Warper::initialize::<CubicBSpline, LongitudeLatitude, LambertConformalConic>(
+        &source_bounds,
+        &target_bounds,
+    )?;
+
+    let source_raster = Array2::zeros((3, 3));
+    let result = warper.warp(&source_raster);
+
+    assert!(result.is_err());
 
     Ok(())
 }
