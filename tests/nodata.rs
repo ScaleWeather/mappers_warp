c798b6afe3f3fa513c367965f8641e00431ff792
Quba1 <22771850+Quba1@users.noreply.github.com>
2024-05-13 14:23:48 +0200

    test non finite result

--- tests/nodata.rs
+++ tests/nodata.rs
@@ -142,7 +142,6 @@ fn nan_discard() -> Result<()> {
     source_raster.slice_mut(s![18..25, 19..24]).fill(f64::NAN);
     source_raster.slice_mut(s![13..15, 21..23]).fill(f64::NAN);
 
-
     let target_raster = warper.warp_discard_nodata(&source_raster)?;
     let ref_raster: Array2<f64> = ndarray_npy::read_npy("./tests/data/waves_nan_discard_ref.npy")?;
 
@@ -150,6 +149,36 @@ fn nan_discard() -> Result<()> {
     Zip::from(&target_raster)
         .and(&ref_raster)
         .map_collect(|&f, &o| assert_approx_eq!(f64, f, o, epsilon = 1e-6));
+
+    Ok(())
+}
+
+#[test]
+fn non_finite_result() -> Result<()> {
+    let src_proj = LongitudeLatitude;
+    let tgt_proj =
+        LambertConformalConic::new(80., 24., 12.472955, 35.1728044444444, Ellipsoid::WGS84)?;
+
+    let source_bounds = RasterBounds::new((60.00, 68.25), (31.75, 40.0), 0.25, 0.25, src_proj)?;
+    let target_bounds = RasterBounds::new(
+        (2_320_000. - 4_000_000., 2_740_000. - 4_000_000.),
+        (5_090_000. - 4_000_000., 5_640_000. - 4_000_000.),
+        10_000.,
+        10_000.,
+        tgt_proj,
+    )?;
+
+    let warper = Warper::initialize::<CubicBSpline, LongitudeLatitude, LambertConformalConic>(
+        &source_bounds,
+        &target_bounds,
+    )?;
+
+    let mut source_raster: Array2<f64> = ndarray_npy::read_npy("./tests/data/waves_34.npy")?;
+    source_raster.slice_mut(s![13..15, 21..23]).fill(f64::MAX);
+
+    assert!(warper.warp_discard_nodata(&source_raster).is_err());
+    assert!(warper.warp_reject_nodata(&source_raster).is_err());
+    assert!(warper.warp_ignore_nodata(&source_raster).is_err());
 
     Ok(())
 }
